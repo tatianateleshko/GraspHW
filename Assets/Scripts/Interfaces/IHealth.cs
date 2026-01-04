39cf1c4a246a5fb307017696977817ab73c569b1
Teleshko Tatiana <dev.tatiana@vivexgames.com>
2026-01-04 21:30:35 +0300

    Added IHealth

---
+++ Assets/Scripts/Interfaces/IHealth.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShootEmUp
+{
+    public interface IHealth 
+    {
+        public void TakeDamage(int value);
+    }
+
+}
